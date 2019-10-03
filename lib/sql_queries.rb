f18583d0e3c04ac8c3c34ddae49248cde9626b12
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 23:34:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,11 +25,11 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, pledges.amount - projects.funding_goal
+  "SELECT projects.title, SUM(pledges.amount) - SUM(projects.funding_goal)
     FROM projects
     JOIN pledges
     ON pledges.id = projects.id
-    WHERE (pledges.amount - projects.funding_goal) > 0
+    GROUP BY projects HAVING SUM(pledges.amount - projects.funding_goal) > 0
 
   "
 end
