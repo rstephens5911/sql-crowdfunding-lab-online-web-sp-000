e756b08420f4a59d4f5de3f94cf392f071bead29
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 23:09:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,6 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   JOIN pledges
   ON projects.id = pledges.id
+  WHERE pledges.amount - projects.funding_goal > 0
   "
 end
 
