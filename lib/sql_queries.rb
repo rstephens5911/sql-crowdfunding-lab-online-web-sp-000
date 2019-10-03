a768c34a2d2fa41105cdd1051258a30c3ec78b29
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 22:22:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -29,7 +29,7 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   FROM projects
   INNER JOIN pledges
   ON projects.id = pledges.id
-  WHERE projects.funding_goal <= pledges.amount
+  WHERE projects.funding_goal <= pledges.amount - projects.funding_goal
 
   "
 end
