1a1151b27dfa46c30a3f48ece3ef650019285b2f
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 23:44:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -32,12 +32,6 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
     GROUP BY projects.id HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
-
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  age INTEGER
-);
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
   "SELECT users.name, SUM(pledges.amount)
   FROM users
