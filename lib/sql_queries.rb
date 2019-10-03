27a1e5e531709cd26c23033268f94c18d856c0c6
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 23:16:36 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, pledges.amount - projects.funding_goal
   FROM projects
-  LEFT OUTER JOIN pledges
+  JOIN pledges
   ON projects.id = pledges.id
   "
 end
