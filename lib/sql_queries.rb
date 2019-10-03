8aec4fcc10e142f81a5df7df84a8cce352aeb71b
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-03 23:30:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,10 +27,7 @@ end
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
     FROM projects
-    INNER JOIN pledges
-    ON projects.id = pledges.id
-    JOIN pledges ON projects.id = pledges.id
-    GROUP BY project_id.title
+    GROUP BY projects.title HAVING (pledges.amount - projects.funding_goal) > 0
 
   "
 end
