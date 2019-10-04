7c56f7b59aae913c376b94388090e0da8f8d489c
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-04 00:12:07 +0000

    Done.

--- spec/crowdfunder_spec.rb
+++ spec/crowdfunder_spec.rb
@@ -95,11 +95,14 @@ describe "crowdfunder sql" do
     end
 
     it '#selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title' do
-      expect(@db.execute(selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title)).to eq([["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise", 170], ["I have bed bugs!", 740], ["I want to teach English in China", 200], ["Iguana needs tail operation", 1035.5], ["My book on SQL", 20], ["The next Harry Potter", 120], ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]])
+      expect(@db.execute(selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title)).to eq([["Animal shelter needs dog food", 210], ["Help me buy a guitar", 98], ["Help save birds of paradise", 170], ["I have bed bugs!", 740], ["I want to teach English in China", 200], ["Iguana needs tail operation", 1035.5], ["My book on SQL", 20], ["The next Harry Potter", 120],
+       ["The next Inna-Gadda-Davida", 342], ["Voldement needs a body", 489]])
     end
 
     it '#selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name' do
-      expect(@db.execute(selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name)).to eq([["Albus", 113, 470], ["Alex", 33, 20], ["Amanda", 24, 40], ["Bear", 6, 50], ["Ena", 24, 100], ["Finnebar", 17, 70], ["Franz", 100, 90], ["Hermione", 30, 50], ["Iguana", 4, 10], ["Katie", 24, 170], ["Marisa", 24, 24], ["Pacha", 5, 60], ["Rosey", 9, 50], ["Sirius", 36, 19], ["Sophie", 24, 60], ["Squid", 5, 270], ["Swizzle", 4, 12], ["Victoria", 23, 1700], ["Voldemort", 90, 34], ["Whale", 6, 125.5]])
+      expect(@db.execute(selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name)).to eq([["Albus", 113, 470], ["Alex", 33, 20], ["Amanda", 24, 40], ["Bear", 6, 50], ["Ena", 24, 100], ["Finnebar", 17, 70],
+       ["Franz", 100, 90], ["Hermione", 30, 50], ["Iguana", 4, 10], ["Katie", 24, 170], ["Marisa", 24, 24], ["Pacha", 5, 60], ["Rosey", 9, 50],
+       ["Sirius", 36, 19], ["Sophie", 24, 60], ["Squid", 5, 270], ["Swizzle", 4, 12], ["Victoria", 23, 1700], ["Voldemort", 90, 34], ["Whale", 6, 125.5]])
     end
 
     it '#selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal' do
