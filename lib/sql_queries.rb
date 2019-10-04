248da6291a3b0e0affc84f5dd406e89a07522197
Ronald Stephens <rstephens@bluemorpho.cc>
2019-10-04 00:06:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -9,7 +9,7 @@
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
   "SELECT projects.title, SUM(pledges.amount)
   FROM projects
-  INNER JOIN pledges
+  JOIN pledges
   ON pledges.project_id = projects.id
   GROUP BY projects.title
   ORDER BY(projects.title) ASC"
